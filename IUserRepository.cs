0e3ea8242513f412e360741e2b8dbd9b1a7bee46
Datbt <datbuithanh1711@gmail.com>
2024-11-07 17:54:50 +0700

    hh

---
+++ IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using KoiShowManagementSystem.Repositories.Entities;
+
+namespace KoiShowManagementSystem.Repositories.Interface
+{
+	public interface IUserRepository
+	{
+		Task<List<User>> GetAllUse();
+        Boolean DelUser(int Id);
+        Boolean DelUser(User user);
+        Boolean AddUser(User user);
+        Boolean UpdtUser(User user);
+        Task<User> GetUserById(int Id);
+    }
+}
+
